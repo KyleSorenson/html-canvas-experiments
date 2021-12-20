946212d626533a4030d52724c81d4fda7b01b92d
KyleSorenson <92221089+KyleSorenson@users.noreply.github.com>
2021-12-20 09:36:24 -0700

    Update dragtest.js

--- pages/dragtest/dragtest.js
+++ pages/dragtest/dragtest.js
@@ -34,27 +34,67 @@ function Circle(ctx, r, [x, y], [h, s, l]) {
 let circle = new Circle(context, 50, [...windowCenter], [60, 50, 60]);
 circle.draw();
 
+let dragStart;
+let dragEnd;
 
-window.addEventListener('mousedown', event => {
+// let mouseDownCoordinates = {
+//   x: 0,
+//   y: 0
+// }
 
+let prevX = 0;
+let circleInitX = circle.x;
+
+window.addEventListener('mousedown', e => {
+  // mouseDownCoordinates = {
+  //   x: e.clientX,
+  //   y: e.clientY
+  // }
+  prevX = e.clientX;
   canvas.style.cursor = "move";
   window.addEventListener('mousemove', clickAndDrag);
 
 })
 
-window.addEventListener('mouseup', event => {
-
+window.addEventListener('mouseup', e => {
   canvas.style.cursor = 'default';
   window.removeEventListener('mousemove', clickAndDrag);
+  circleInitX = circle.x;
 
 })
 
 function clickAndDrag(e) {
+  // let circleInitPos = {
+  //   x: circle.x,
+  //   y: circle.y
+  // }
+  // let coordinateOffset = {
+  //   x: e.clientX - mouseDownCoordinates.x,
+  //   y: e.clientY - mouseDownCoordinates.y
+  // }
+  let mouseMoveX = e.clientX;
+  let newX = mouseMoveX - prevX;
+  // let circleInitX = circle.x;
   if (e.clientX >= 0 && e.clientX <= frameWidth && e.clientY >=0 && e.clientY <= frameHeight) {
     clearFrame(context);
-    circle.x = e.clientX;
-    circle.y = e.clientY;
+    circle.x = circleInitX + newX;
+    // circle.y = coordinateOffset.y;
     circle.draw();
-    console.log(`[${e.clientX},${e.clientY}]`);
+    // console.log(`
+    // MouseDown: [${mouseDownCoordinates.x},${mouseDownCoordinates.y}]
+    // Offset: [${coordinateOffset.x},${coordinateOffset.y}]
+    // Circle: [${circle.x += coordinateOffset.x},${circle.x += coordinateOffset.x}]
+    // `);
+    context.fillText(`Mouse Down: ${prevX}`, 10, 20);
+    context.fillText(`Mouse Move: ${mouseMoveX}`, 10, 35);
+    context.fillText(`Offset: ${newX}`, 10, 50);
+    context.fillText(`Circle Start: ${circleInitX}`, 10, 65);
+    context.fillText(`Circle Current: ${circle.x}`, 10, 80);
+    // console.log(`
+    // MouseDown: ${prevX},
+    // Offset: ${newX},
+    // Circle: ${circle.x}
+    // `);
   }
+  // prevX = newX;
 }
